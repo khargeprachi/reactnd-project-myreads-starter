42a0c3e18a7e30ca3a6da465d8713c9eed766361
khargeprachi <kharge.prachi@gmail.com>
2018-06-08 23:36:45 +0530

    Add files via upload

--- src/BookList.js
+++ src/BookList.js
@@ -25,7 +25,10 @@ class BookList extends Component {
                     <li key={book.title}>
                       <div className="book">
                         <div className="book-top">
+                        {book.imageLinks && (
+
                           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        ) }
                           <div className="book-shelf-changer">
                             <select  defaultValue={book.shelf} onChange={(event) => this.props.change(event.target.value,book)}>
                               <option value="none" disabled>Move to...</option>
@@ -37,8 +40,9 @@ class BookList extends Component {
                           </div>
                         </div>
                         <div className="book-title">{book.title}</div>
-                        {  book.authors.map((author)=> (
+                        {book.authors && (  book.authors.map((author)=> (
                           <div className="book-authors" key={author}>{author}</div>
+                        )
                         ))
                         }
                       </div>
